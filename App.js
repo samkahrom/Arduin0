202290d6bb12dd179ebe3c26f648ffcc0933fa3c
Sam Kahrom <sam.kahrom@myy.haaga-helia.fi>
2020-09-07 15:47:31 +0300

    Update App.js

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View, Image } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Image source={{ uri: 'https://i.imgur.com/FN0naH5.png' }}
+      <Image source={{ uri: 'https://i.imgur.com/6EyuKgK.png' }}
         style={{ width: 200, height: 200 }} />
       <StatusBar style="auto" />
     </View>
