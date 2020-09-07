e6a121ab6f52f0a1f54ff6952fc54e5c2c3e992f
Sam Kahrom <sam.kahrom@myy.haaga-helia.fi>
2020-09-07 13:32:52 +0300

    Update App.js

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Moro olen Arduin0</Text>
+      <Image source={{ uri: 'https://i.imgur.com/FN0naH5.png' }}
+        style={{ width: 200, height: 200 }} />
       <StatusBar style="auto" />
     </View>
   );
@@ -14,7 +15,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#130c18',
     alignItems: 'center',
     justifyContent: 'center',
   },
